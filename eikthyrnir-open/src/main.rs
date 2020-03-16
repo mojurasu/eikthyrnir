7f3de340ca9d83063938d6f05204763eb3c37d01
SitiSchu <admin@sitischu.com>
2020-03-16 01:48:59 +0100

    Add open
    Signed-off-by: SitiSchu <admin@sitischu.com>

---
+++ eikthyrnir-open/src/main.rs
@@ -0,0 +1,26 @@
+use eikthyrnir::*;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+mod error;
+use error::Result;
+
+/// Open anything
+///
+/// Useful on windows since there's nothing like xdg-open there
+#[derive(StructOpt, Debug)]
+pub struct Opt {
+    /// Open the specified item or the current working directory
+    item: Option<String>
+}
+
+pub fn main() -> Result<()> {
+    let opt: Opt = Opt::from_args();
+    let to_open = match opt.item {
+        Some(i) => i,
+        None => ".".to_string(),
+    };
+    padded_message("Opening".bright_blue().bold(), &to_open);
+    open::that(to_open)?;
+    Ok(())
+}
