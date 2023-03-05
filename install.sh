9a9d2c79f9a9f83c59e082cb8c3c83b6f8b149a5
SitiSchu <admin@sitischu.com>
2023-03-05 21:49:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-cargo install --path eikthyrnir
-cargo install --path web
-cargo install --path open
-cargo install --path chance
+#!/usr/bin/env bash
+cargo install --path eikthyrnir || exit
+cargo install --path web || exit
+cargo install --path open || exit
+cargo install --path chance || exit
