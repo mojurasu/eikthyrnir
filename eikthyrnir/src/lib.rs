f692fb2403e2c8c4e8a7f16f58b8a52b3f71235e
SitiSchu <admin@sitischu.com>
2023-03-05 21:48:56 +0100

    Add messy subcommand enumeration

--- eikthyrnir/src/lib.rs
+++ eikthyrnir/src/lib.rs
@@ -3,10 +3,10 @@ pub use colored::Colorize;
 
 pub const PADDING: usize = 15;
 
-pub fn padded_message<T: Into<String>>(title: ColoredString, msg: T) -> () {
+pub fn padded_message<T: Into<String>>(title: ColoredString, msg: T) {
     println!("{:>padding$} {}", title, msg.into(), padding = PADDING)
 }
 
-pub fn print_error<T: Into<String>>(msg: T) -> () {
+pub fn print_error<T: Into<String>>(msg: T) {
     eprintln!("{:>padding$} {}", "Error".bright_red().bold(), msg.into(), padding = PADDING)
 }
--- eikthyrnir/src/main.rs
+++ eikthyrnir/src/main.rs
@@ -1,7 +1,13 @@
 use std::env;
 use std::ffi::OsString;
 use std::process::Command;
+
 use anyhow::Result;
+
+// #[cfg(target_os = "windows")]
+// const ENV_PATH_SEPERATOR: char = ';';
+// #[cfg(not(target_os = "windows"))]
+// const ENV_PATH_SEPERATOR: char = ':';
 
 fn main() -> Result<()> {
     env::set_var("CLICOLOR_FORCE", "1");
@@ -21,7 +27,55 @@ fn main() -> Result<()> {
             }
         }
         _ => {
-            eikthyrnir::print_error("missing subcommand")
+            let mut available = Vec::new();
+
+
+            if let Some(d) = env::current_exe()?.parent() {
+                for e in d.read_dir()?.filter_map(|e| e.ok()) {
+                    if e.file_type()?.is_file() {
+                        if let Ok(filename) = e.file_name().into_string() {
+                            #[cfg(target_os = "windows")]
+                            if !filename.ends_with(".exe") {
+                                continue;
+                            }
+                            if filename.starts_with(env!("CARGO_PKG_NAME")) {
+                                #[cfg(target_os = "windows")]
+                                    let filename = filename.strip_suffix(".exe").unwrap();
+                                // TODO: Make this nice, can fail when executable has a hyphen
+                                let cmd_name = filename.split("-").skip(1).next().unwrap().to_string();
+                                available.push(cmd_name)
+                            }
+                        }
+                    }
+                }
+            }
+
+            if available.is_empty() {
+                eikthyrnir::print_error("missing subcommand");
+            } else {
+                eikthyrnir::print_error(format!("missing subcommand. choose from the following: {}", available.join(", ")));
+            }
+            // TODO: Below is the technically correct implementation, however, it needs to filter out non-executables
+            // for p in env::var("PATH")?.split(ENV_PATH_SEPERATOR).map(PathBuf::from_str).filter_map(|e| e.ok()) {
+            //     // TODO: Need to expand ~ for ~/.cargo/bin
+            //     if p.starts_with("~") {
+            //         println!("{:?}", PathBuf::from(p.components().skip(1)));
+            //     }
+            //     if let Ok(r) = p.read_dir() {
+            //         for e in r.filter_map(|e| e.ok()) {
+            //             if e.file_type()?.is_file() {
+            //                 if let Ok(filename) = e.file_name().into_string() {
+            //                     if filename.starts_with(env!("CARGO_PKG_NAME")) && filename.ends_with("") {
+            //                         // eikthyrnir::padded_message("".bright_blue(), filename)
+            //                     }
+            //                 }
+            //
+            //             }
+            //
+            //         }
+            //
+            //     }
+            // }
         }
     };
     Ok(())
